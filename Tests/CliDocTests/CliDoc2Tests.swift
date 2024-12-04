46c2c7ad315db58f896fc0a357aab5443e0adf0c
Michael Housh <mhoush@houshhomeenergy.com>
2024-12-04 08:01:23 -0500

    pull from main

---
+++ Tests/CliDocTests/CliDoc2Tests.swift
@@ -0,0 +1,34 @@
+@testable import CliDoc2
+@preconcurrency import Rainbow
+import Testing
+
+let setupRainbow: Bool = {
+  Rainbow.enabled = true
+  Rainbow.outputTarget = .console
+  return true
+}()
+
+@Test
+func testGroup() {
+  #expect(setupRainbow)
+  let group = Group {
+    Label { "Foo:" }
+    "Bar"
+    "Baz"
+    Note { "Bang:" } content: { "boom" }
+    if setupRainbow {
+      Label("Hello, rainbow").color(.blue)
+    } else {
+      Label("No color for you!").color(.red)
+    }
+  }
+  .color(.green)
+  .style(.italic)
+
+  print(type(of: group))
+  print(group.render())
+
+//   let note = Note { "Bang:" } content: { "boom" }
+//   print(note.render())
+//   print(type(of: note.label))
+}
