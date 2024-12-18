bbace73d974fd3e6985461431692bea773c7c5d8
Michael Housh <mhoush@houshhomeenergy.com>
2024-12-18 09:07:14 -0500

    feat: Updates platforms in package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "swift-cli-doc",
+  platforms: [.macOS(.v10_15)],
   products: [
     .library(name: "CliDocCore", targets: ["CliDocCore"]),
     .library(name: "CliDoc", targets: ["CliDoc"])
